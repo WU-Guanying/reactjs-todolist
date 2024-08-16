e20dd6a0fc98c6da99bafd6cd43bb0b95eeea81e
WU Guanying <guanying.x.wu@gmail.com>
2024-08-16 16:38:41 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react"
 import TodoList from "./components/TodoList"
-import TodoInput from "./components/Todoinput"
+import TodoInput from "./components/TodoInput"
 
 function App() {
   
